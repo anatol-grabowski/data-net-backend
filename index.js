fcb59da13e9092458f9cc2e874d699dcf6cb5c00
tot <grabantot@gmail.com>
2018-07-21 14:07:54 +0300

    added eslint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { start } = require('./lib/datanet-backend')
 
 start()
-.catch(err => {
-  console.log(err)
-  process.exit(1)
-})
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
