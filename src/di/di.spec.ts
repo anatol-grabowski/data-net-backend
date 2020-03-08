e6947914eec127aedd0e756872a154e775119b4b
tot <grabantot@gmail.com>
2020-03-08 17:50:30 +0300

    fixed docker-compose.yml

--- src/di/di.spec.ts
+++ src/di/di.spec.ts
@@ -1,11 +1,18 @@
 import { processModuleRecursive, resolveModule, resolveProviderRecursive, depsToStr, initModule } from './di'
 import { MainModule } from '../modules/main'
 
-console.log(JSON.stringify(MainModule, null, 2))
 
-const res = resolveModule(MainModule)
-console.log(res)
-console.log(res.map(r => depsToStr(r)).join('\n'))
+async function main() {
+  console.log(JSON.stringify(MainModule, null, 2))
+
+  const res = resolveModule(MainModule)
+  console.log(res)
+  console.log(res.map(r => depsToStr(r)).join('\n'))
+  const mod = await initModule(MainModule)
+  console.log(mod)
+  await mod.services.UserRepository.create()
+}
+
+main()
 
 
-initModule(MainModule)
--- src/modules/user/services/user.repository.ts
+++ src/modules/user/services/user.repository.ts
@@ -1,12 +1,16 @@
-import { Db } from 'mongodb'
+import { Db, Collection } from 'mongodb'
 
 export class UserRepository {
+  private collection: Collection
+
   constructor(
     private readonly mongodbDbConnection: Db,
-  ) {}
+  ) {
+    this.collection = this.mongodbDbConnection.collection('users')
+  }
 
-  async create() {
-
+  async create(user) {
+    this.collection.insertOne({ u: 33 })
   }
 
   async findByUsername(username: string) {
